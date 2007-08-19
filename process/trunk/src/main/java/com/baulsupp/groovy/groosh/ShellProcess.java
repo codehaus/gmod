4e82c28e492d578bf43ac1b8e67add0fcfd899be
eggeral <eggeral@f908d50e-8e36-0410-ac0a-93754bd5008d>
2007-08-19 15:22:39 +0000

    Added license and author information to all classes

--- process/trunk/src/main/java/com/baulsupp/groovy/groosh/ShellProcess.java
+++ process/trunk/src/main/java/com/baulsupp/groovy/groosh/ShellProcess.java
@@ -1,3 +1,17 @@
+//  !!projectname!! -- Provides a shell-like capability for handling external processes
+//
+//  Copyright © 2004 Yuri Schimke
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is
+//  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied. See the License for the specific language governing permissions and limitations under the
+//  License.
+
 package com.baulsupp.groovy.groosh;
 
 import java.io.IOException;
@@ -7,6 +21,11 @@ import com.baulsupp.process.ProcessFactory;
 import com.baulsupp.process.Sink;
 import com.baulsupp.process.Source;
 
+/**
+ * 
+ * @author Yuri Schimke
+ *
+ */
 public class ShellProcess extends GrooshProcess {
 	private AppProcess process = null;
 	private String[] args;
--- process/trunk/src/main/java/com/baulsupp/process/IOUtil.java
+++ process/trunk/src/main/java/com/baulsupp/process/IOUtil.java
@@ -1,3 +1,17 @@
+//  !!projectname!! -- Provides a shell-like capability for handling external processes
+//
+//  Copyright © 2004 Yuri Schimke
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is
+//  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied. See the License for the specific language governing permissions and limitations under the
+//  License.
+
 package com.baulsupp.process;
 
 import java.io.IOException;
@@ -9,6 +23,11 @@ import java.util.concurrent.Future;
 
 import com.baulsupp.groovy.groosh.ThreadPerTaskExecutorService;
 
+/**
+ * 
+ * @author Yuri Schimke
+ *
+ */
 public class IOUtil {
 	private static final int BUFFER_SIZE = 8192;
 
