8b4e748b42d202002dcad142b6cc5dc0d447763b
melix <melix@f908d50e-8e36-0410-ac0a-93754bd5008d>
2007-12-20 17:48:49 +0000

    Upgraded for Groovy 1.5. Compatibility with JDK 1.4. Package name moved to org.codehaus.groovy. Build now managed by Maven.

--- gfreemarker/trunk/plugins/urlencoder.groovy
+++
@@ -1,7 +0,0 @@
-import com.lingway.groovy.text.freemarker.IGroovyFreeMarkerPlugin
-
-class urlencoder implements IGroovyFreeMarkerPlugin {
-	String transform(Map params, String content) {
-		URLEncoder.encode(content);
-	}
-}
