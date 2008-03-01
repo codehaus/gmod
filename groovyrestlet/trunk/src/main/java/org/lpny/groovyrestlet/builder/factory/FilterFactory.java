c7a5191f488f51f62bde4f9e0957325e6524c7c7
keke <keke@f908d50e-8e36-0410-ac0a-93754bd5008d>
2008-03-01 14:06:03 +0000

    upgrade to Restlet 1.1-M2

--- groovyrestlet/trunk/src/main/java/org/lpny/groovyrestlet/builder/factory/FilterFactory.java
+++ groovyrestlet/trunk/src/main/java/org/lpny/groovyrestlet/builder/factory/FilterFactory.java
@@ -21,75 +21,75 @@ import org.restlet.data.Response;
  * @since 0.1
  */
 public class FilterFactory extends RestletFactory {
-    protected static final String AFTER  = "after";
-    protected static final String BEFORE = "before";
+	protected static final String	AFTER	= "after";
+	protected static final String	BEFORE	= "before";
 
-    protected static final String HANDLE = "handle";
+	protected static final String	HANDLE	= "handle";
 
-    public FilterFactory() {
-        super("filter");
-    }
+	public FilterFactory() {
+		super("filter");
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    protected Object newInstanceInner(final FactoryBuilderSupport builder,
-            final Object name, final Object value, final Map attributes)
-            throws InstantiationException, IllegalAccessException {
-        final Closure before = (Closure) attributes.remove(BEFORE);
-        final Closure after = (Closure) attributes.remove(AFTER);
-        final Closure handle = (Closure) attributes.remove(HANDLE);
-        return new Filter(FactoryUtils.getParentRestletContext(builder)) {
+	@SuppressWarnings("unchecked")
+	@Override
+	protected Object newInstanceInner(final FactoryBuilderSupport builder,
+	        final Object name, final Object value, final Map attributes)
+	        throws InstantiationException, IllegalAccessException {
+		final Closure before = (Closure) attributes.remove(BEFORE);
+		final Closure after = (Closure) attributes.remove(AFTER);
+		final Closure handle = (Closure) attributes.remove(HANDLE);
+		return new Filter(FactoryUtils.getParentRestletContext(builder)) {
 
-            @Override
-            protected void afterHandle(final Request request,
-                    final Response response) {
-                if (after != null) {
-                    after.call(FactoryUtils.packArgs(this, after, request,
-                            response));
-                } else {
-                    super.afterHandle(request, response);
-                }
-            }
+			@Override
+			protected void afterHandle(final Request request,
+			        final Response response) {
+				if (after != null) {
+					after.call(FactoryUtils.packArgs(this, after, request,
+					        response));
+				} else {
+					super.afterHandle(request, response);
+				}
+			}
 
-            @Override
-            protected void beforeHandle(final Request request,
-                    final Response response) {
-                if (before != null) {
-                    before.call(FactoryUtils.packArgs(this, before, request,
-                            response));
-                } else {
-                    super.beforeHandle(request, response);
-                }
-            }
+			@Override
+			protected int beforeHandle(final Request request,
+			        final Response response) {
+				if (before != null) {
+					return (Integer) before.call(FactoryUtils.packArgs(this,
+					        before, request, response));
+				} else {
+					return super.beforeHandle(request, response);
+				}
+			}
 
-            @Override
-            protected void doHandle(final Request request,
-                    final Response response) {
-                if (handle != null) {
-                    handle.call(FactoryUtils.packArgs(this, handle, request,
-                            response));
-                } else {
-                    super.doHandle(request, response);
-                }
-            }
+			@Override
+			protected int doHandle(final Request request,
+			        final Response response) {
+				if (handle != null) {
+					return (Integer) handle.call(FactoryUtils.packArgs(this,
+					        handle, request, response));
+				} else {
+					return super.doHandle(request, response);
+				}
+			}
 
-        };
-    }
+		};
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.lpny.groovyrestlet.builder.factory.AbstractFactory#setChildInner(groovy.util.FactoryBuilderSupport,
-     *      java.lang.Object, java.lang.Object)
-     */
-    @Override
-    protected Object setChildInner(final FactoryBuilderSupport builder,
-            final Object parent, final Object child) {
-        if (child != null) {
-            if (child instanceof Restlet) {
-                ((Filter) parent).setNext((Restlet) child);
-            }
-        }
-        return null;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.lpny.groovyrestlet.builder.factory.AbstractFactory#setChildInner(groovy.util.FactoryBuilderSupport,
+	 *      java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	protected Object setChildInner(final FactoryBuilderSupport builder,
+	        final Object parent, final Object child) {
+		if (child != null) {
+			if (child instanceof Restlet) {
+				((Filter) parent).setNext((Restlet) child);
+			}
+		}
+		return null;
+	}
 }
