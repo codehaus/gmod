663860774a9e6b41dbb2061bac3b21aac0781ec5
keke <keke@f908d50e-8e36-0410-ac0a-93754bd5008d>
2008-05-17 07:45:06 +0000

    upgraded to version 0.3-SNAPSHOT
    fix two issues reported by Bruno
    
    1. Using context class loader in FactoryUtil
    2. Root path of Directory now only accepts valid URI

--- groovyrestlet/trunk/src/test/java/org/lpny/groovyrestlet/builder/factory/DirectoryFactoryTest.java
+++ groovyrestlet/trunk/src/test/java/org/lpny/groovyrestlet/builder/factory/DirectoryFactoryTest.java
@@ -20,32 +20,53 @@ import org.testng.annotations.Test;
  */
 @Test(groups = { "unittest" })
 public class DirectoryFactoryTest extends AbstractFactoryTest {
-    private DirectoryFactory fixture;
+	private DirectoryFactory	fixture;
 
-    @BeforeTest(groups = { "unittest" })
-    @Test(groups = { "unittest" })
-    public void construct() {
-        fixture = new DirectoryFactory();
-        assert fixture.getName().equals("directory");
-    }
+	@BeforeTest(groups = { "unittest" })
+	@Test(groups = { "unittest" })
+	public void construct() {
+		fixture = new DirectoryFactory();
+		assert fixture.getName().equals("directory");
+	}
 
-    public void testNewInstance() throws InstantiationException,
-            IllegalAccessException {
-        final FactoryBuilderSupport builder = createMockBuilder();
-        final Map attributes = new HashMap();
-        attributes.put("root", ".");
-        final Directory directory = (Directory) fixture.newInstance(builder,
-                "directory", null, attributes);
-        assert directory != null;
-    }
+	@Test(groups = { "unittest" }, expectedExceptions = { IllegalArgumentException.class })
+	public void testNewInstanceNullRoot() throws InstantiationException,
+	        IllegalAccessException {
+		final FactoryBuilderSupport builder = createMockBuilder();
+		final Map attributes = new HashMap();
+		final Directory directory = (Directory) fixture.newInstance(builder,
+		        "directory", null, attributes);
+		assert directory != null;
+	}
 
-    @Test(groups = { "unittest" }, expectedExceptions = { IllegalArgumentException.class })
-    public void testNewInstanceNullRoot() throws InstantiationException,
-            IllegalAccessException {
-        final FactoryBuilderSupport builder = createMockBuilder();
-        final Map attributes = new HashMap();
-        final Directory directory = (Directory) fixture.newInstance(builder,
-                "directory", null, attributes);
-        assert directory != null;
-    }
+	public void testNewInstanceWithRelativePath()
+	        throws InstantiationException, IllegalAccessException {
+		final FactoryBuilderSupport builder = createMockBuilder();
+		final Map attributes = new HashMap();
+		attributes.put("root", "file:.");
+		final Directory directory = (Directory) fixture.newInstance(builder,
+		        "directory", null, attributes);
+		assert directory != null;
+	}
+
+	@Test(groups = { "unittest" }, expectedExceptions = { IllegalArgumentException.class })
+	public void testNewInstanceWithRootIsNotURL()
+	        throws InstantiationException, IllegalAccessException {
+		final FactoryBuilderSupport builder = createMockBuilder();
+		final Map attributes = new HashMap();
+		attributes.put("root", ".");
+		final Directory directory = (Directory) fixture.newInstance(builder,
+		        "directory", null, attributes);
+		assert directory != null;
+	}
+
+	public void testNewInstanceWithURL() throws InstantiationException,
+	        IllegalAccessException {
+		final FactoryBuilderSupport builder = createMockBuilder();
+		final Map attributes = new HashMap();
+		attributes.put("root", "clap://class/test");
+		final Directory directory = (Directory) fixture.newInstance(builder,
+		        "directory", null, attributes);
+		assert directory != null;
+	}
 }
