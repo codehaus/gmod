ab3113c3be99103bd9e2437736a2f825b64a7854
rocketnia <rocketnia@f908d50e-8e36-0410-ac0a-93754bd5008d>
2008-06-17 06:57:46 +0000

    overloaded isCase and boolean operators on CumulativeExpressionValidator and provided more constructors

--- science/trunk/science/src/org/codehaus/groovy/science/CumulativeExpressionValidator.java
+++ science/trunk/science/src/org/codehaus/groovy/science/CumulativeExpressionValidator.java
@@ -25,14 +25,20 @@ public class CumulativeExpressionValidator implements ExpressionValidator
 		Restriction,
 		Exception;
 	}
+
 	
-	
+	/**
+	 * <p>The default value to return when validating an expression that is
+	 * neither explicitly accepted nor explicitly rejected by any of the
+	 * filters.</p>
+	 */
+	private boolean defaultValue;
+		
 	/**
 	 * <p>The conditions set by each of the incremental changes made to the
 	 * {@code validates} method, in the order those changes were registered.</p>
 	 */
 	private List< Object > filters;
-	
 	
 	/**
 	 * <p>The types of each of the incremental changes made to the
@@ -47,9 +53,60 @@ public class CumulativeExpressionValidator implements ExpressionValidator
 	 */
 	public CumulativeExpressionValidator()
 	{
+		this( true );
+	}
+	
+	/**
+	 * <p>Constructs a {@code CumulativeExpressionValidator} that either accepts
+	 * all expressions or rejects all expressions.</p>
+	 * 
+	 * @param defaultValue
+	 *     {@code true} if the new {@code CumulativeExpressionValidator} should
+	 *     initially accept all expressions; {@code false} if it should reject
+	 *     all expressions
+	 */
+	public CumulativeExpressionValidator( boolean defaultValue )
+	{
+		this.defaultValue = defaultValue;
+		
 		filters      = new ArrayList< Object >();
 		filterTypes  = new ArrayList< FilterType >();
 	}
+	
+	/**
+	 * <p>Duplicates the given {@code CumulativeExpressionValidator}.</p>
+	 * 
+	 * @param other  the {@code CumulativeExpressionValidator} to duplicate
+	 */
+	public CumulativeExpressionValidator( CumulativeExpressionValidator other )
+	{
+		defaultValue = other.defaultValue;
+		
+		filters      = new ArrayList< Object >(      other.filters      );
+		filterTypes  = new ArrayList< FilterType >(  other.filterTypes  );
+	}
+	
+	/**
+	 * <p>Constructs a {@code CumulativeExpressionValidator} that accepts
+	 * exactly those expressions that cause the given object's {@code isCase}
+	 * method in Groovy to return true.</p>
+	 * 
+	 * @param defaultValue  the initial behavior of the {@code validates} method
+	 */
+	public CumulativeExpressionValidator( Object defaultValue )
+	{
+		this( true );
+		
+		if ( defaultValue instanceof Boolean )
+		{
+			this.defaultValue = ((Boolean)defaultValue).booleanValue();
+		}
+		else
+		{
+			allowOnly( defaultValue );
+		}
+	}
+	
 	
 	/**
 	 * <p>Incrementally change the {@code validates} method by registering a
@@ -114,19 +171,11 @@ public class CumulativeExpressionValidator implements ExpressionValidator
 	 * @see org.codehaus.groovy.science.ExpressionValidator#validates(
 	 *     org.codehaus.groovy.science.SymbolicExpression
 	 * )
-	 * 
-	 * @throws ClassCastException
-	 *     only if at least one of the registered filters returns something
-	 *     other than {@code true} or {@code false}
-	 * 
-	 * @throws MissingMethodException
-	 *     only if at least one of the registered filters does not accept a
-	 *     single argument of type {@code SymbolicExpression}
 	 */
 	@Override
 	public boolean validates( SymbolicExpression expression )
 	{
-		boolean result = true;
+		boolean result = defaultValue;
 		
 		for ( int filterIndex = 0; filterIndex < filters.size(); filterIndex++ )
 		{
@@ -152,5 +201,123 @@ public class CumulativeExpressionValidator implements ExpressionValidator
 		}
 		
 		return result;
+	}
+	
+	/**
+	 * <p>Checks the given expression against this
+	 * {@code CumulativeExpressionValidator}'s criteria and returns whether the
+	 * criteria are satisfied.</p>
+	 * 
+	 * <p>This is an alias for {@code validates( SymbolicExpression )}.</p>
+	 * 
+	 * @see org.codehaus.groovy.science.ExpressionValidator#validates(
+	 *     org.codehaus.groovy.science.SymbolicExpression
+	 * )
+	 * 
+	 * @param switchValue  the expression to validate
+	 * 
+	 * @return
+	 *     {@code true} if the given expression fits the criteria; {@code false}
+	 *     otherwise
+	 */
+	public boolean isCase( SymbolicExpression switchValue )
+	{
+		return validates( switchValue );
+	}
+	
+	/**
+	 * <p>Returns a new {@code CumulativeExpressionValidator} that accepts
+	 * all of the expressions this validator accepts as well as all expressions
+	 * accepted by the given object's {@code isCase} method in Groovy.</p>
+	 * 
+	 * @param other  the case to take a disjunction with
+	 * 
+	 * @return
+	 *     the result of taking the disjunction of this validator and the given
+	 *     case
+	 */
+	public CumulativeExpressionValidator or( Object other )
+	{
+		CumulativeExpressionValidator result =
+			new CumulativeExpressionValidator( this );
+		
+		result.allowAlso( other );
+		
+		return result;
+	}
+	
+	/**
+	 * <p>Returns a new {@code CumulativeExpressionValidator} that accepts
+	 * exactly those expressions this validator accepts that are also accepted
+	 * by the given object's {@code isCase} method in Groovy.</p>
+	 * 
+	 * @param other  the case to take a conjunction with
+	 * 
+	 * @return
+	 *     the result of taking the conjunction of this validator and the given
+	 *     case
+	 */
+	public CumulativeExpressionValidator and( Object other )
+	{
+		CumulativeExpressionValidator result =
+			new CumulativeExpressionValidator( this );
+		
+		result.allowOnly( other );
+		
+		return result;
+	}
+	
+	/**
+	 * <p>Returns a new {@code CumulativeExpressionValidator} that accepts
+	 * exactly those expressions with are accepted by this validator or the
+	 * given object's {@code isCase} method in Groovy, but not both.</p>
+	 * 
+	 * @param other  the case to take a symmetric difference with
+	 * 
+	 * @return
+	 *     the result of taking the symmetric difference of this validator and
+	 *     the given case
+	 */
+	public CumulativeExpressionValidator xor( Object other )
+	{
+		final CumulativeExpressionValidator self = this;
+		final Object that = other;
+		
+		return new CumulativeExpressionValidator( new Object() {
+			
+			@SuppressWarnings("unused")
+            public boolean isCase( SymbolicExpression switchValue )
+			{
+				return (
+					self.validates( switchValue )
+					^
+					((Boolean)InvokerHelper.invokeMethod(
+						that,
+						"isCase",
+						new Object[]{ switchValue }
+					)).booleanValue()
+				);
+			}
+		} );
+	}
+	
+	/**
+	 * <p>Returns a new {@code CumulativeExpressionValidator} that accepts
+	 * exactly those expressions this validator does not accept.</p>
+	 * 
+	 * @return  the result of taking the complement of this validator
+	 */
+	public CumulativeExpressionValidator bitwiseNegate()
+	{
+		final CumulativeExpressionValidator self = this;
+		
+		return new CumulativeExpressionValidator( new Object() {
+			
+			@SuppressWarnings("unused")
+            public boolean isCase( SymbolicExpression switchValue )
+			{
+				return( !self.validates( switchValue ) );
+			}
+		} );
 	}
 }
