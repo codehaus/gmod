0bab00ce5941141958e997df8acfba4466eac17f
rocketnia <rocketnia@f908d50e-8e36-0410-ac0a-93754bd5008d>
2008-06-24 11:14:28 +0000

    added static utility methods to ConstantOperator

--- science/trunk/science/src/org/codehaus/groovy/science/ConstantOperator.java
+++ science/trunk/science/src/org/codehaus/groovy/science/ConstantOperator.java
@@ -1,6 +1,10 @@
 package org.codehaus.groovy.science;
 
 
+import groovy.lang.Closure;
+import java.util.ArrayList;
+
+
 /**
  * An operator intended for use as a constant value in a
  * {@code SymbolicExpression}.
@@ -17,7 +21,7 @@ public class ConstantOperator< T >
 	private T value;
 	
 	/**
-	 * Constructs a {@code ConstantOperator} that wraps the given constant
+	 * Creates a {@code ConstantOperator} that wraps the given constant
 	 * value.
 	 * 
 	 * @param value  the constant value to wrap
@@ -30,6 +34,101 @@ public class ConstantOperator< T >
 			throw new NullPointerException();
 		
 		this.value = value;
+	}
+	
+	/**
+	 * <p>Creates a {@code SymbolicExpression} that represents the given
+	 * constant value. This is done using {@code ConstantOperator}.</p>
+	 * 
+	 * @param value  the value for the expression to represent
+	 * 
+	 * @return  an expression representing the given value
+	 * 
+	 * @throws NullPointerException  if {@code value} is {@code null}
+	 */
+	public static SymbolicExpression con( Object value )
+	{
+		return new SymbolicExpression(
+			new ConstantOperator< Object >( value ),
+			new ArrayList< SymbolicExpression >()
+		);
+	}
+	
+	/**
+	 * <p>Retrieves the constant value that a {@code SymbolicExpression}
+	 * using {@code ConstantOperator} represents.</p>
+	 * 
+	 * @param expression  the constant expression
+	 * 
+	 * @return  the value represented by the constant expression
+	 * 
+	 * @throws IllegalArgumentException
+	 *     if {@code expression} is non-{@code null} but is not a nullary
+	 *     expression with a {@code ConstantOperator} as its operator
+	 * 
+	 * @throws NullPointerException  if {@code expression} is {@code null}
+	 */
+	public static Object unCon( SymbolicExpression expression )
+	{
+		if ( expression == null )
+			throw new NullPointerException();
+		
+		Object operator = expression.getOperator();
+		
+		if (
+			!(operator instanceof ConstantOperator)
+			||
+			!expression.getArgumentList().isEmpty()
+		)
+			throw new IllegalArgumentException();
+		
+		return ((ConstantOperator< ? >)operator).getValue();
+	}
+	
+	/**
+	 * <p>Wraps the given {@code Closure} to create a corresponding
+	 * {@code Closure} that accepts constant {@code SymbolicExpression}s and
+	 * returns a constant {@code SymbolicExpression}, where the constant values
+	 * of all of these expressions are values that the original {@code Closure}
+	 * would have accepted and returned.</p>
+	 * 
+	 * <p>For instance, in Groovy, {@code inCon( { a, b -> a + b } )} should
+	 * result in a {@code Closure} that is functionally identical to
+	 * {@code { a, b -> con( unCon( a ) + unCon( b ) ) }}. In fact, those
+	 * {@code Closure}s should throw the same exceptions when given the same
+	 * parameters.</p>
+	 * 
+	 * @param simpleClosure  the {@code Closure} to wrap
+	 * 
+	 * @return  the wrapped {@code Closure}
+	 * 
+	 * @throws IllegalArgumentException
+	 *     if {@code expression} is non-{@code null} but is not a nullary
+	 *     expression with a {@code ConstantOperator} as its operator
+	 * 
+	 * @throws NullPointerException  if {@code expression} is {@code null}
+	 */
+	public static Closure inCon( Closure simpleClosure )
+	{
+		if ( simpleClosure == null )
+			throw new NullPointerException();
+		
+		final Closure finalSimpleClosure = simpleClosure; 
+		
+		return new Closure( null )
+		{
+			@SuppressWarnings("unused")
+            public SymbolicExpression doCall( SymbolicExpression... arguments )
+			{
+				Object[] argumentValues = new Object[ arguments.length ];
+				for ( int index = 0; index < arguments.length; index++ )
+				{
+					argumentValues[ index ] = unCon( arguments[ index ] );
+				}
+				
+				return con( finalSimpleClosure.call( argumentValues ) );
+			}
+		};
 	}
 	
 	/* (non-Javadoc)
--- science/trunk/science/src/org/codehaus/groovy/science/tests/ConstantOperatorTests.groovy
+++ science/trunk/science/src/org/codehaus/groovy/science/tests/ConstantOperatorTests.groovy
@@ -4,6 +4,8 @@ package org.codehaus.groovy.science.tests
 import org.codehaus.groovy.science.ConstantOperator
 import org.codehaus.groovy.science.SymbolicExpression
 
+import static org.codehaus.groovy.science.ConstantOperator.*
+
 
 class ConstantOperatorTests extends GroovyTestCase
 {
@@ -20,5 +22,37 @@ class ConstantOperatorTests extends GroovyTestCase
 		assertEquals( threepio, threepio );
 		assertEquals( threepio, c( [ 3, "P", 0 ] ) );
 		assertToString( threepio, "<< (Constant: [3, P, 0]): [] >>" );
+	}
+	
+	void testCon()
+	{
+		// Make sure that {@code con}, {@code unCon}, and {@code inCon} work
+		// properly.
+		
+		def dummy = new SymbolicExpression( "dummy", [] );
+		
+		
+		shouldFail( NullPointerException, { con( null ) } );
+		shouldFail( NullPointerException, { unCon( null ) } );
+		shouldFail( NullPointerException, { inCon( null ) } );
+		
+		shouldFail( IllegalArgumentException, { unCon( dummy ); } );
+		
+		assertEquals(
+			con( [ 3, "P", 0 ] ),
+			new SymbolicExpression( new ConstantOperator( [ 3, "P", 0 ] ), [] )
+		);
+		
+		assertEquals( unCon( con( [ 3, "P", 0 ] ) ), [ 3, "P", 0 ] );
+		
+		assertEquals(
+			inCon( { a, b -> a + b } )( con( 1 ), con( 2 ) ),
+			con( 3 )
+		);
+		
+		shouldFail(
+			IllegalArgumentException,
+			{ inCon( { a, b -> a + b } )( dummy, dummy ) }
+		);
 	}
 }
