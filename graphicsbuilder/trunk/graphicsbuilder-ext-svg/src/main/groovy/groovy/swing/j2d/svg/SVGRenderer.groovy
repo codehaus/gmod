3122c81a43b717cee92a9591bd35110abbbb7296
aalmiray <aalmiray@f908d50e-8e36-0410-ac0a-93754bd5008d>
2008-07-09 00:38:38 +0000

    added useful render* methods to SVGrenderer

--- graphicsbuilder/trunk/graphicsbuilder-ext-svg/src/main/groovy/groovy/swing/j2d/svg/SVGRenderer.groovy
+++ graphicsbuilder/trunk/graphicsbuilder-ext-svg/src/main/groovy/groovy/swing/j2d/svg/SVGRenderer.groovy
@@ -24,7 +24,6 @@ import org.apache.batik.dom.svg.SVGDOMImplementation
 import org.w3c.dom.Document
 import org.w3c.dom.DOMImplementation
 
-
 /**
  * @author Andres Almiray <aalmiray@users.sourceforge.net>
  */
@@ -34,7 +33,93 @@ final class SVGRenderer {
     public SVGRenderer(){
        gb = new GraphicsBuilder()
     }
-
+    
+    /**
+     * Writes an svg document to a string.<br>
+     *
+     * @param width the width of the svg document
+     * @param height the height of the svg document
+     * @param closure a closure containg GraphicsBuilder's nodes
+     *
+     * @throws IOException if the string can't be writen.
+     */
+    public String render( int width, int height, Closure closure ) {
+       return render( width, height, gb.group(closure) )
+    }
+    
+    /**
+     * Writes an svg document to a string.<br>
+     *
+     * @param width the width of the svg document
+     * @param height the height of the svg document
+     * @param go any GraphicsOperation
+     *
+     * @throws IOException if the string can't be writen.
+     */
+    public String render( int width, int height, GraphicsOperation go ) {
+       def sw = new StringWriter()
+       renderToWriter( sw, width, height, go )
+       return sw.buffer
+    }
+    
+    /**
+     * Writes an svg document to a writer.<br>
+     *
+     * @param writer a Writer
+     * @param width the width of the svg document
+     * @param height the height of the svg document
+     * @param closure a closure containg GraphicsBuilder's nodes
+     *
+     * @throws IOException if the writer can't be writen to.
+     */
+    public void renderToWriter( Writer writer, int width, int height, Closure closure ) {
+       renderToWriter( writer, width, height, gb.group(closure) )
+    }
+    
+    /**
+     * Writes an svg document to a writer.<br>
+     *
+     * @param writer a Writer
+     * @param width the width of the svg document
+     * @param height the height of the svg document
+     * @param go any GraphicsOperation
+     *
+     * @throws IOException if the writer can't be writen to.
+     */
+    public void renderToWriter( Writer writer, int width, int height, GraphicsOperation go ) {
+       def svgGenerator = createGraphics( width, height, go )
+       svgGenerator.stream( writer )
+    }
+    
+    /**
+     * Writes an svg document to a stream.<br>
+     *
+     * @param stream an OutputStream
+     * @param width the width of the svg document
+     * @param height the height of the svg document
+     * @param closure a closure containg GraphicsBuilder's nodes
+     *
+     * @throws IOException if the stream can't be writen to.
+     */
+    public void renderToStream( OutputStream stream, int width, int height, Closure closure ) {
+       renderToStream( stream, width, height, gb.group(closure) )
+    }
+    
+    /**
+     * Writes an svg document to a stream.<br>
+     *
+     * @param stream an OutputStream
+     * @param width the width of the svg document
+     * @param height the height of the svg document
+     * @param go any GraphicsOperation
+     *
+     * @throws IOException if the stream can't be writen to.
+     */
+    public void renderToStream( OutputStream stream, int width, int height, GraphicsOperation go ) {
+       def svgGenerator = createGraphics( width, height, go )
+       svgGenerator.stream( new OutputStreamWriter(stream,"UTF-8") )
+    }
+    
     /**
      * Writes an svg document to a file.<br>
      * Assumes that the filename follows the unix conventions ("/" as file separator)
@@ -46,10 +131,9 @@ final class SVGRenderer {
      *
      * @throws IOException if the file can't be created and writen to.
      */
-    public void renderToFile( String filename, int width, int height, Closure closure ){
-       renderToFile( filename, width, height, gb.group(closure) )
+    public File renderToFile( String filename, int width, int height, Closure closure ) {
+       return renderToFile( filename, width, height, gb.group(closure) )
     }
-     
 
     /**
      * Writes an image to a file.<br>
@@ -62,7 +146,7 @@ final class SVGRenderer {
      *
      * @throws IOException if the file can't be created and writen to.
      */
-    public void renderToFile( String filename, int width, int height, GraphicsOperation go ){     
+    public File renderToFile( String filename, int width, int height, GraphicsOperation go ) {     
        assert filename : "Must define a value for filename"
         
        filename -= ".svg"
@@ -75,12 +159,17 @@ final class SVGRenderer {
           parentDir.mkdirs()
        }
        
+       def file = new File(filename+".svg")
+       renderToStream( new FileOutputStream(file), width, height, go )
+       return file
+    }
+     
+    private SVGGraphics2D createGraphics( int width, int height, GraphicsOperation go ) {
        // Get a DOMImplementation.
        DOMImplementation domImpl = SVGDOMImplementation.getDOMImplementation()
 
        // Create an instance of org.w3c.dom.Document.
-       String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI
-       Document document = domImpl.createDocument(svgNS, "svg", null)
+       Document document = domImpl.createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null)
 
        // Create an instance of the SVG Generator.
        SVGGraphics2D svgGenerator = new SVGGraphics2D(document)
@@ -92,9 +181,6 @@ final class SVGRenderer {
        context.g.setSVGCanvasSize( new Dimension(width,height) )
        go.execute(context)
 
-       // Finally, stream out SVG to the standard output using
-       // UTF-8 encoding.
-       Writer out = new OutputStreamWriter(new FileOutputStream(filename+".svg"), "UTF-8")
-       svgGenerator.stream(out)
+       return svgGenerator
     }
 }
