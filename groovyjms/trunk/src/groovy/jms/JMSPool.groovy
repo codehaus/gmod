e3b23188f5cc0d31b1e6ca2d44f901592766a80b
mingfai <mingfai@f908d50e-8e36-0410-ac0a-93754bd5008d>
2008-10-16 17:06:49 +0000

    refactored

--- groovyjms/trunk/src/groovy/jms/JMSPool.groovy
+++ groovyjms/trunk/src/groovy/jms/JMSPool.groovy
@@ -5,21 +5,23 @@ import groovy.jms.provider.ActiveMQPooledJMSProvider
 
 /**
  * JMSPool extends JMS and provided configurable JMS Pooling. Unlike the "raw" JMS, you don't need to provide a connection
- * or session to it.
+ * or session to it. It aims at providing something similar to Spring DefaultMessageListenerContainer 
  *
  * "A Session object is a single-threaded context for producing and consuming messages", in order to enjoy better
  * throughput in both incoming and outgoing messaging, you are recommended to use the JMSPool over JMS. JMSPool could
  * be configured to establish multiple session in mutiple thread to consume or produce messages.
  *
- * The JMSPool utilize ActiveMQ SessionPool
+ * The JMSPool utilize ActiveMQ SessionPool for sending message.
+ *
+ *
  *
  */
 class JMSPool extends JMS {
     private static final defaultCfg = [:]
 
-    JMSPool(Closure c) {
+    JMSPool(Map cfg = null, Closure c) {
         super(null, null, c)
-        this.provider = new ActiveMQPooledJMSProvider();
+        this.provider = new ActiveMQPooledJMSProvider(cfg);
         this.factory = provider.getConnectionFactory()
     }
 
