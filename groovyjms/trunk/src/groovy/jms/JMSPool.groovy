c6c11d56d71228f176a5aa8c7589cbdbf1fed4b9
mingfai <mingfai@f908d50e-8e36-0410-ac0a-93754bd5008d>
2008-10-17 12:33:25 +0000

    refactored JMS.onMessage

--- groovyjms/trunk/src/groovy/jms/JMSPool.groovy
+++ groovyjms/trunk/src/groovy/jms/JMSPool.groovy
@@ -2,6 +2,9 @@ package groovy.jms
 
 import javax.jms.ConnectionFactory
 import groovy.jms.provider.ActiveMQPooledJMSProvider
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
 
 /**
  * JMSPool extends JMS and provided configurable JMS Pooling. Unlike the "raw" JMS, you don't need to provide a connection
@@ -13,18 +16,37 @@ import groovy.jms.provider.ActiveMQPooledJMSProvider
  *
  * The JMSPool utilize ActiveMQ SessionPool for sending message.
  *
- *
- *
+ * Read the following before using JMSPool
+ * 1. for message consumption, order of messages are not guaranteed after using the pool
+ * 2. there is no support for Transaction and there is no recovery mechanism (yet), use Spring JMS or Jencks if you need those features
+ * 3. JMSPool is implemented as one thread per connection-session
  */
 class JMSPool extends JMS {
-    private static final defaultCfg = [:]
+    ExecutorService pool;
+    int poolSize = 10;
 
     JMSPool(Map cfg = null, Closure c) {
         super(null, null, c)
-        this.provider = new ActiveMQPooledJMSProvider(cfg);
+        this.provider = new ActiveMQPooledJMSProvider(cfg)
         this.factory = provider.getConnectionFactory()
+        poolSize = cfg.'poolSize' ?: poolSize
+        pool = Executors.newFixedThreadPool(poolSize);
     }
 
-    def init() {}
+    def init() {} // TODO refactor JMS and JMSPool to the same level using a new parent class
+
+    List<Future> futures = []
+
+    def onMessage(Map dest, Map cfg = null, Object target) {
+        if (cfg && !cfg.containsKey('threads')) {
+            return super.onMessage(dest, cfg, target)
+        }
+
+        cfg.'threads'.times { i ->
+            println i
+
+        }
+
+    }
 
 }
--- groovyjms/trunk/test/groovy/jms/JMSPoolTest.groovy
+++ groovyjms/trunk/test/groovy/jms/JMSPoolTest.groovy
@@ -7,7 +7,12 @@ class JMSPoolTest extends GroovyTestCase {
     void testConstructor() {
         def pool = new JMSPool()
         assertNotNull pool?.factory
-        println pool?.factory 
-        assertTrue (pool?.factory instanceof PooledConnectionFactory)
+        println pool?.factory
+        assertTrue(pool?.factory instanceof PooledConnectionFactory)
+    }
+
+    void testOnMessage() {
+        def pool = new JMSPool()
+        pool.onMessage(topic: 'testTopic', threads: 10)
     }
 }
