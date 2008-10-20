f34721e41a4404076c3f5d22dde80d8189f6fcd5
mingfai <mingfai@f908d50e-8e36-0410-ac0a-93754bd5008d>
2008-10-20 14:42:26 +0000

    added MapMessage toMap()

---
+++ groovyjms/trunk/src/groovy/jms/JMSUtils.java
@@ -0,0 +1,20 @@
+package groovy.jms;
+
+import javax.jms.MapMessage;
+import javax.jms.JMSException;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Enumeration;
+
+public class JMSUtils {
+    public static Map extractMapFromMapMessage(MapMessage mm) throws JMSException {
+        if (mm == null) return null;
+        Map m = new HashMap();
+        Enumeration mmFields = mm.getMapNames();
+        while (mmFields.hasMoreElements()) {
+            String k = (String) mmFields.nextElement();
+            m.put(k, mm.getObject(k));
+        }
+        return m;
+    }
+}
