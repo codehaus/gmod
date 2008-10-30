24a1f07179c382dd4eb627ae7a4ad4657fcf5d5b
mingfai <mingfai@f908d50e-8e36-0410-ac0a-93754bd5008d>
2008-10-30 09:01:50 +0000

    added send reply message support and other changes

--- groovyjms/trunk/src/groovy/jms/JMSPool.groovy
+++ groovyjms/trunk/src/groovy/jms/JMSPool.groovy
@@ -7,6 +7,7 @@ import java.util.concurrent.*
 import javax.jms.ConnectionFactory
 import javax.jms.MessageListener
 import org.apache.log4j.Logger
+import javax.jms.Message
 
 /**
  * JMSPool extends JMS and provided configurable JMS Pooling. Unlike the "raw" JMS, you don't need to provide a connection
@@ -83,31 +84,59 @@ class JMSPool extends AbstractJMS {
 
     def send(Map spec) {
         if (threadPool.isShutdown()) throw new IllegalStateException("JMSPool has been shutdown already")
-        if (logger.isTraceEnabled()) logger.trace("send() - spec: $spec")
+        if (!spec) throw new IllegalArgumentException("spec shall not be null")
+        if (!spec.keySet().any {it in JMS.SEND_DEST_PARAMS}) throw new IllegalArgumentException("send() - required dest params is not found. required: ${JMS.SEND_DEST_PARAMS}")
+        if (!spec.containsKey('message') && !spec.containsKey('data')) throw new IllegalArgumentException("send message must have a \"message\"")
+        if (spec.containsKey('reply') && !(spec.'reply' instanceof Message)) throw new IllegalArgumentException("reply must take a JMS Message as value, reply.class: ${spec.'reply'.getClass()}, reply: ${spec.'reply'}")
+        if (spec.containsKey('reply') && !(spec.'reply'.getJMSReplyTo())) throw new IllegalArgumentException("replying message must have a JMSReplyTo destination. reply: ${spec.'reply'}")
 
-        threadPool.submit({
-            if (logger.isTraceEnabled()) logger.trace("send() - executing submitted job - jms? ${JMSThread.jms.get() != null}")
-            if (spec.'delay') sleep(spec.'delay')
-            JMSThread.jms.get().send(spec)
-            JMSThread.jms.get().connect()
-        } as Runnable)
+        boolean sync = (spec.'sync') ?: false;
+
+        if (spec.containsKey('threads') && spec.'threads' > 1) { //multiple thread mode
+
+            def dests = threadPool.submit({
+                def result = []
+                def q = spec.'toQueue' ?: spec.'queue', t = spec.'toTopic' ?: spec.'topic', d = spec.'toDest' ?: spec.'dest'
+                if (q && q instanceof Collection) { result += q.collect {JMS.getThreadLocal().createQueue(it)}}
+                else if (q) { result << JMS.getThreadLocal().createQueue(q)}
+                if (t && t instanceof Collection) { result += t.collect {JMS.getThreadLocal().createTopic(it)}}
+                else if (t) { result << JMS.getThreadLocal().createTopic(t)}
+                if (d && d instanceof Collection) { result += d} else if (d) { result << d}
+                return result;
+            } as Callable).get()
+
+            throw new UnsupportedOperationException("not implemented")
+        } else { // single thread mode
+            Future result = threadPool.submit({
+                if (logger.isTraceEnabled()) logger.trace("send() - begin - spec: $spec, jms? ${JMSThread.jms.get() != null}")
+                if (spec.'delay') sleep(spec.'delay')
+                JMSThread.jms.get().send(spec)
+                JMSThread.jms.get().connect()
+                return true;
+            } as Callable)
+            return (sync) ? result.get() : result;
+        }
     }
 
     def receive(Map spec, Closure with = null) {     // spec.'timeout'
         if (threadPool.isShutdown()) throw new IllegalStateException("JMSPool has been shutdown already")
-        if (logger.isTraceEnabled()) logger.trace("receive() - spec: $spec, with? ${with != null}")
+        //if (logger.isTraceEnabled()) logger.trace("receive() - spec: $spec, with? ${with != null}")
         Future receiveJob = threadPool.submit({
             if (logger.isTraceEnabled()) logger.trace("receive() - executing submitted job - jms? ${JMSThread.jms.get() != null}")
             //TODO break down every destination-selector to a thread
             //TODO handle the 'threads' parameter
             def result = JMSThread.jms.get().receive(spec, with);
             if (logger.isTraceEnabled()) logger.trace("receive() - executed job - result: $result, thread-jms: ${JMSThread.jms.get()}")
-            return result;
+            return result; //TODO can't return
         } as Callable);
+
+        if (with) return; //no need to wait for result if with closure is provided
+
         if (spec.containsKey('timeout')) {
             return receiveJob.get(spec.'timeout', TimeUnit.MILLISECONDS)
         } else {
             return receiveJob.get();
         }
     }
+
 }
