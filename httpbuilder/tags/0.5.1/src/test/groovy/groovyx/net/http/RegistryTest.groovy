057dcd28fb2fbd3d6cbcda9f39a13029fa5e167a
tomstrummer <tomstrummer@f908d50e-8e36-0410-ac0a-93754bd5008d>
2010-10-01 02:05:25 +0000

    Release tag for v0.5.1.

---
+++ httpbuilder/tags/0.5.1/src/test/groovy/groovyx/net/http/RegistryTest.groovy
@@ -0,0 +1,190 @@
+package groovyx.net.http
+
+import org.apache.http.ProtocolVersion
+import org.apache.http.entity.StringEntity
+import org.apache.http.message.BasicHttpResponse
+import org.junit.Testimport java.io.StringReaderimport java.io.ByteArrayInputStream
+import static groovyx.net.http.ContentType.*
+/**
+ * @author tnichols
+ */
+public class RegistryTest {
+
+	@Test public void testParserRegistry() {
+		def reg = new ParserRegistry();
+		
+		assert reg.defaultParser
+		reg.each { assert it.key && it.value }
+
+		assert reg."text/plain"
+		assert reg["text/plain"]
+		
+		def newParser = {}
+		reg."application/json" = newParser
+		assert reg['application/json'].is( newParser )
+		
+		reg[ ContentType.JSON ] = newParser
+		assert reg[ "application/javascript" ].is( newParser )
+		
+		reg.defaultParser = newParser
+		assert newParser.is( reg.defaultParser )
+	}
+
+	@Test public void testEncoderAccessors() {
+		def reg = new EncoderRegistry();
+		
+		reg.each { assert it.key && it.value }
+
+		assert reg."text/plain"
+		assert reg["text/plain"]
+		
+		def newEnc = {}
+		reg."application/json" = newEnc
+		reg['application/xml'] = newEnc
+		assert reg['application/json'].is( newEnc )
+		assert reg."application/xml".is( newEnc )
+	}
+	
+	@Test public void testXMLEncoder() {
+		def reg = new EncoderRegistry();
+		
+		def entity = reg.encodeXML {
+			xml( AAA: 'aaa' ) {
+				one 'one'
+				two 'two'
+			}
+		}
+		
+		assert entity.contentType.value == "application/xml"
+//		println entity.content.text
+		assert entity.content.text == "<xml AAA='aaa'><one>one</one><two>two</two></xml>"
+		
+		entity = reg.encodeXML( '<xml AAA="aaa"><one>one</one><two>two</two></xml>' )
+		assert entity.content.text == '<xml AAA="aaa"><one>one</one><two>two</two></xml>'
+		def value = 'something'
+		entity = reg.encodeXML( "<xml AAA='aaa'><one>$value</one><two>two</two></xml>" )
+		assert entity.content.text == "<xml AAA='aaa'><one>something</one><two>two</two></xml>"
+	}
+	
+	@Test public void testCharsetAndText() {
+		def reg = new EncoderRegistry( charset: "ISO-8859-1" );
+		
+		def entity = reg.encodeText { out ->
+			out << "This is a test"
+		}
+		
+		assert entity.contentType.value == 'text/plain'
+		assert entity.content.getText('ISO-8859-1') == "This is a test"
+		assert entity.content.getText('utf-16') != "This is a test"
+		
+		def w = { it << "this is a test 1" } as Writable
+		entity = reg.encodeText( w )
+		assert entity.content.getText('ISO-8859-1') == "this is a test 1"
+		
+		entity = reg.encodeText( "This is a test 2" )
+		assert entity.contentType.value == 'text/plain'
+		assert entity.content.getText('ISO-8859-1') == "This is a test 2"
+
+		entity = reg.encodeText( new StringReader( "This is a test 3\nMore text") )
+		assert entity.contentType.value == 'text/plain'		
+		assert entity.content.getText('ISO-8859-1') == "This is a test 3\nMore text"
+	}
+	
+	@Test public void testStream() {
+		def reg = new EncoderRegistry();
+		
+		def data = [ 0x0, 0x1, 0x2 ] as byte[]
+		
+		def entity = reg.encodeStream { out ->  // closure
+			out << data
+		}
+		
+		assert entity.contentType.value == 'application/octet-stream'
+		assert entity.contentLength == data.length
+		def result = new ByteArrayOutputStream() 
+		result << entity.content
+		assert result.toByteArray() == data
+		
+		entity = reg.encodeStream( new ByteArrayInputStream(data) )
+		assert entity.contentLength == data.length
+		result = new ByteArrayOutputStream() 
+		result << entity.content
+		assert result.toByteArray() == data
+		
+		entity = reg.encodeStream( data ) // byte[]
+		assert entity.contentLength == data.length
+		result = new ByteArrayOutputStream() 
+		result << entity.content
+		assert result.toByteArray() == data
+		
+		entity = reg.encodeStream( result ) // ByteArrayOutputStream
+		assert entity.contentLength == data.length
+		result = new ByteArrayOutputStream() 
+		result << entity.content
+		assert result.toByteArray() == data
+	}
+	
+	@Test public void testJSONEncoder() {
+		def reg = new EncoderRegistry();
+		
+		def entity = reg.encodeJSON( [
+				first : [ one:1, two:"2" ],
+				second : 'some string'
+		] )
+		
+		assert entity.contentType.value == "application/json"
+//		println entity.content.text
+		assert entity.content.text == '{"first":{"one":1,"two":"2"},"second":"some string"}'
+		
+		entity = reg.encodeJSON( ["first", "second", 3, [map:4] ] )
+  		assert entity.content.text == '["first","second",3,{"map":4}]'
+  		
+  		entity = reg.encodeJSON {
+			root {
+				first {
+					one = 1
+					two = '2'
+				}
+				second = 'some string'
+			}
+		}
+		
+//		println entity.content.text
+		assert entity.content.text == '{"root":{"first":{"one":1,"two":"2"},"second":"some string"}}'
+		
+		entity = reg.encodeJSON( '["first","second",3,{"map":4}]' )
+  		assert entity.content.text == '["first","second",3,{"map":4}]'
+		def another = 'second'
+		entity = reg.encodeJSON( "['first','$another',3,{'map':4}]" )
+  		assert entity.content.text == "['first','second',3,{'map':4}]"
+	}
+	
+	@Test public void testFormEncoder() {
+		def enc = new EncoderRegistry()
+		
+		def param1 = "p1"
+		def entity = enc.encodeForm( ["${param1}":'one', p2:['two','three']] )
+		
+		assert entity.contentType.elements[0].name == 'application/x-www-form-urlencoded'
+		assert entity.content.text == "p1=one&p2=two&p2=three"
+		
+		entity = enc.encodeForm( "p1=goober&p2=something+else" )
+		assert entity.contentType.value == 'application/x-www-form-urlencoded'
+		assert entity.content.text == "p1=goober&p2=something+else"
+	}
+	
+	@Test public void testFormParser() {
+		def parser = new ParserRegistry()
+
+		def entity = new StringEntity( "p1=goober&p2=something+else", "utf-8" )
+		// GMOD-137: URLENC parsing doesn't work w/ bad content-type
+		entity.setContentType "text/plain" // NOT application/x-www-form-urlencoded
+		
+		def response = new BasicHttpResponse( new ProtocolVersion( "HTTP", 1, 1 ), 200, "OK" )
+		response.entity = entity
+		def map = parser.parseForm( response )
+		assert map
+		assert map.p1 == 'goober'
+		assert map.p2 == 'something else' 
+	}
+}
---
+++ httpbuilder/tags/0.5.1/src/test/groovy/groovyx/net/http/URIBuilderTest.groovy
@@ -0,0 +1,185 @@
+package groovyx.net.http
+
+import org.junit.Test
+
+/**
+ * @author tnichols
+ */
+public class URIBuilderTest {
+	
+	@Test public void testConstructors() {
+		def uri 
+		try { uri = new URIBuilder( null ) }
+		catch( ex ) { /* Expected exception */ }
+
+		def string = 'http://google.com/search/q?one=1'
+		uri = new URIBuilder( new URI( string ) )
+		assert uri.toString() == string
+		
+		uri = new URIBuilder( new URL( string ) )
+		assert uri.toString() == string
+		
+		uri = new URIBuilder( string )
+		assert uri.toString() == string
+		
+		def uri2 = URIBuilder.convertToURI( uri )
+		assert uri2 == uri.toURI()
+	}
+	
+	@Test public void testCloneAndEquals() {
+		def string = 'http://google.com/search/q?one=1'
+		def uri = new URIBuilder( string )
+		def u2 = uri.clone()
+		assert u2 == uri
+		assert u2.toString() == uri.toString()
+	}
+	
+	@Test public void testTypecast() {
+		def string = 'http://google.com/search/q?one=1'
+		def uri = new URIBuilder( string )
+		
+		def u2 = uri as URI
+		assert ( u2 instanceof URI )
+		assert u2.toString() == uri.toString()
+
+		u2 = uri as URL
+		assert ( u2 instanceof URL )
+		assert u2.toString() == uri.toString()
+
+		u2 = uri as String
+		assert ( u2 instanceof String )
+		assert u2 == uri.toString()
+		
+		u2 = new URIBuilder( new URI(string) )
+		assert u2 == uri
+		assert u2.toString() == uri.toString()
+		
+		u2 = new URIBuilder( new URL(string) )
+		assert u2 == uri
+		assert u2.toString() == uri.toString()
+	}
+	
+	@Test public void testPath() {
+		def uri = new URIBuilder( 'http://localhost/p1/p2?a=1&b=2&c=3#frag' )
+	
+		uri.path = 'p2a'
+		assert uri.toString() == 'http://localhost/p1/p2a?a=1&b=2&c=3#frag'
+		
+		uri.path = 'p2/p3/'
+		assert uri.toString() == 'http://localhost/p1/p2/p3/?a=1&b=2&c=3#frag'
+
+		uri.path = 'p4'
+		assert uri.toString() == 'http://localhost/p1/p2/p3/p4?a=1&b=2&c=3#frag'
+
+		uri.path = '../../p2b'
+		assert uri.toString() == 'http://localhost/p1/p2b?a=1&b=2&c=3#frag'
+		
+		def p = 'p5'
+		uri.path = "/p4/$p"
+		assert uri.toString() == 'http://localhost/p4/p5?a=1&b=2&c=3#frag'
+	}
+	
+	@Test public void testPathEscaping() {
+		def uri = new URIBuilder( 'http://localhost/' )
+		uri.path = "/p1 p2"
+		assert uri.toString() == 'http://localhost/p1%20p2'
+		
+		uri = new URIBuilder( 'http://localhost/p1?one=1#frag' )
+		uri.path = "/p1 p2 p3"
+		assert uri.toString() == 'http://localhost/p1%20p2%20p3?one=1#frag'
+	}
+	
+	// When params are added to a path, does it goober up the escaped-then-unescaped path?
+	@Test public void testPathEscaping2() {
+		def uri = new URIBuilder( 'http://johannburkard.de/%22bla%22' )
+		uri.query = [ what_is_this: 'i_dont_even' ]
+
+		uri = new URIBuilder( 'http://codehaus.org/')
+		uri.path = '"bla"'
+		
+		uri.fragment = 'what evs'
+		assert uri.toString() == 'http://codehaus.org/%22bla%22#what%20evs'
+		uri.query = [ a: 'b' ]
+		assert uri.toString() == 'http://codehaus.org/%22bla%22?a=b#what%20evs'
+		uri.port = 80
+		uri.host = 'google.com'
+		uri.scheme = 'http'
+	}
+	
+	@Test public void testParams() {
+		def uri = new URIBuilder( 'http://localhost/p1/p2?a=1&b=2&c=3#frag' )
+		assert uri.query.size() == 3
+		assert uri.query.a == '1'
+		assert uri.query.b == '2'
+		assert uri.query.c == '3'
+		
+		uri.addQueryParam 'd', '4'
+		assert uri.query.d == '4'
+		
+		assert uri.hasQueryParam( "d" )
+		
+		uri.removeQueryParam "b"
+		assert ! uri.hasQueryParam( "b" )
+		assert uri.query.b == null
+		uri.addQueryParam 'b', ''
+		
+		uri.addQueryParams( [ e : 5, f : 6, a : 7 ] )
+		
+		def query = uri.query
+		assert query.containsKey( 'b' )
+		assert query.size() == 6
+		assert query == [a:['1','7'], c:'3', d:'4', b:null, e:'5', f:'6']
+
+		uri.query = [z:0,y:9,x:8]
+		assert uri.toString() == 'http://localhost/p1/p2?z=0&y=9&x=8#frag'
+				
+		uri.addQueryParams( z:1, y:2 )
+		assert uri.toString() == 'http://localhost/p1/p2?z=0&y=9&x=8&z=1&y=2#frag'
+				
+		uri.addQueryParam 'y', 'blah'
+		assert uri.toString() == 'http://localhost/p1/p2?z=0&y=9&x=8&z=1&y=2&y=blah#frag'
+
+//		uri.addQueryParam '', 'asdf'
+//		println uri // prints ...p2?=asdf... but apparently that is a valid URI...
+				
+		uri.query = null
+		assert uri.toString() == 'http://localhost/p1/p2#frag'
+		
+		uri.query = [:]
+		assert uri.toString() == 'http://localhost/p1/p2#frag'
+		
+		uri = new URIBuilder( 'http://localhost/p1/p2' )
+		uri.addQueryParam 'z','1'
+		assert uri.query.z == '1'
+	}
+
+	@Test public void testMostEverythingElse() {
+		def url = 'http://www.google.com:80/one/two?a=1#frag'		
+		def uri = new URIBuilder( url )
+		
+		uri.scheme = "https"
+		assert uri.toString() == 'https://www.google.com:80/one/two?a=1#frag'
+				
+		uri.host = "localhost"
+		assert uri.toString() == 'https://localhost:80/one/two?a=1#frag'
+		
+		uri.port = 8080
+		assert uri.toString() == 'https://localhost:8080/one/two?a=1#frag'
+
+		uri.fragment = 'asdf2'
+		assert uri.toString() == 'https://localhost:8080/one/two?a=1#asdf2'
+	}
+	
+	@Test public void testParamEncoding(){
+		def uri = new URIBuilder( 'http://localhost:8080#test' )
+		
+		uri.query = [q:'a:b',z:'y&z']
+		assert 'http://localhost:8080?q=a%3Ab&z=y%26z#test' == uri.toString()
+		
+		uri.query = [q:"war & peace"]
+		assert "http://localhost:8080?q=war+%26+peace#test" == uri.toString()
+						
+		uri.query = ['a&b':'c+d=e']
+		assert "http://localhost:8080?a%26b=c%2Bd%3De#test" == uri.toString() 
+	}
+}
