36a96985577ef037330e71579e5463d20928f34a
malbery <malbery@f908d50e-8e36-0410-ac0a-93754bd5008d>
2010-12-11 21:47:57 +0000

    created project structure, included simple Cateogory classes, one simple test, gradle build file

---
+++ nio2/trunk/src/main/java/groovy/nio/file/PathCategory.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2003-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package groovy.nio.file;
+
+import org.codehaus.groovy.runtime.DefaultGroovyMethodsSupport;
+
+import groovy.nio.channels.ReadableByteChannelCategory;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.file.Path;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CodingErrorAction;
+
+/**
+ * This class defines new Java 7 specific groovy methods which extend the normal
+ * JDK classes inside the Groovy environment. Static methods are used with the
+ * first parameter the destination class.
+ *
+ * @author Merlyn Albery-Speyer
+ */
+public class PathCategory {
+    /**
+     * Read the content of the Path and return it as a String. 
+     * (Only works with JDK1.7 or later).
+     *
+     * @param path a Path whose content we want to read
+     * @return a String containing the content of the path
+     * @throws IOException if an IOException occurs.
+     * @since ??
+     */
+    public static String getText(Path path) throws IOException {
+        return ReadableByteChannelCategory.getText(path.newByteChannel());
+    }
+}
